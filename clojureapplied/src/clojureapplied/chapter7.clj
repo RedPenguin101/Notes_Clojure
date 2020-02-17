d957503769bd07dd936cd696b9763890fff63e05
RedPenguin101 <allaboutthatmace1789@gmail.com>
2020-02-17 17:33:17 -0600

    start c7 clojure applied

---
+++ clojureapplied/src/clojureapplied/chapter7.clj
@@ -0,0 +1,8 @@
+(ns clojureapplied.chapter5)
+
+(comment "chapter 7: Compose your application
+         * Taking things apart
+         * Implementing with component
+         * Putting things together
+         * System config")
+
