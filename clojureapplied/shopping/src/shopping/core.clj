38804254d21796cda013e66c2c3fa93a9f97bbf0
Joe Lezala <allaboutthatmace1789@gmail.com>
2020-02-22 15:52:02 -0600

    Shopping add restocking inventory

--- clojureapplied/shopping/src/shopping/core.clj
+++ clojureapplied/shopping/src/shopping/core.clj
@@ -4,3 +4,14 @@
 
 (store/init {:bacon 10 :eggs 10 :sausage 10})
 (shopper/go-shopping [:bacon :sausage :eggs])
+store/inventory
+;; => #<Atom@48fa28fb: {:bacon 9, :eggs 9, :sausage 9}>
+store/sold-items
+;; => #<Atom@f1dc666: {:bacon 1, :sausage 1, :eggs 1}>
+
+(store/restock-all)
+store/inventory
+;; => #<Atom@7658c469: {:bacon 10, :eggs 10, :sausage 10}>
+
+store/sold-items
+;; => #<Atom@f1dc666: {}>
--- clojureapplied/shopping/src/shopping/store.clj
+++ clojureapplied/shopping/src/shopping/store.clj
@@ -1,6 +1,7 @@
 (ns shopping.store)
 
 (def inventory (atom {}))
+(def sold-items (atom {}))
 
 (defn no-negative-values? [m]
   (not-any? neg? (vals m)))
@@ -8,9 +9,34 @@
 (defn in-stock? [item]
   (and (pos? (item @inventory))))
 
-(defn init [items]
-  (reset! inventory items))
-
 (defn grab [item]
   (when (in-stock? item)
     (swap! inventory update-in [item] dec)))
+
+ (defn restock-order [k r ov nv]
+   (doseq [item (for [kw (keys ov)
+                      :when (not= (kw ov) (kw nv))]
+                  kw)]
+     (swap! sold-items update-in [item] (fnil inc 0))
+     (println "need to restock" item)))
+
+(defn init [m]
+  (reset! inventory m)
+  (reset! sold-items {})
+  (set-validator! inventory no-negative-values?)
+  (add-watch inventory :restock restock-order))
+
+(defn restock-all []
+  (swap! inventory #(merge-with + % @sold-items))
+  (reset! sold-items {}))
+
+(comment
+  (init {:apples 1 :bacon 3 :milk 2})
+  (grab :bacon)
+  (grab :bacon)
+  (grab :milk)
+  sold-items
+  (restock-all)
+  inventory
+  sold-items
+  )
