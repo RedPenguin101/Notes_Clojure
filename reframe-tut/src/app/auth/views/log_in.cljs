863fe0d21ecef6456a2a35de35154db387912cd9
RedPenguin101 <allaboutthatmace1789@gmail.com>
2020-04-25 11:03:59 +0100

    fix nesting - put forms inside cols

--- reframe-tut/src/app/auth/views/log_in.cljs
+++ reframe-tut/src/app/auth/views/log_in.cljs
@@ -8,21 +8,21 @@
         values         (r/atom initial-values)]
     (fn []
       [:> Row {:justify-content "center"}
-       [:> Col {:xs 12 :sm 6}]
-       [page-nav {:center "Log In"}]
-       [:> FormGroup
-        [:> Label {:html-for :email} "Email"]
-        [:> Input {:control   true
-                   :id        :email
-                   :type      "email"
-                   :value     (:email @values)
-                   :on-change #(swap! values assoc :email (.. % -target -value))}]]
-       [:> FormGroup
-        [:> Label {:html-for :password} "Password"]
-        [:> Input {:control   true
-                   :id        :password
-                   :type      "password"
-                   :value     (:password @values)
-                   :on-change #(swap! values assoc :password (.. % -target -value))}]]
+       [:> Col {:xs 12 :sm 6}
+        [page-nav {:center "Log In"}]
+        [:> FormGroup
+         [:> Label {:html-for :email} "Email"]
+         [:> Input {:control   true
+                    :id        :email
+                    :type      "email"
+                    :value     (:email @values)
+                    :on-change #(swap! values assoc :email (.. % -target -value))}]]
+        [:> FormGroup
+         [:> Label {:html-for :password} "Password"]
+         [:> Input {:control   true
+                    :id        :password
+                    :type      "password"
+                    :value     (:password @values)
+                    :on-change #(swap! values assoc :password (.. % -target -value))}]]]
        ]))
   )
