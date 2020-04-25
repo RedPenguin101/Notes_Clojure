1c61a3ec15b3d5d00d23b5dbb376a927314361a5
RedPenguin101 <allaboutthatmace1789@gmail.com>
2020-04-25 09:47:43 +0100

    v9 add subscription for active item

--- reframe-tut/src/app/core.cljs
+++ reframe-tut/src/app/core.cljs
@@ -4,6 +4,7 @@
             [app.db]
             ;; -- nav --
             [app.nav.events]
+            [app.nav.subs]
             [app.theme :refer [cheffy-theme]]
             [app.nav.views.nav :refer [nav]]
             ["@smooth-ui/core-sc" :refer [Normalize Button ThemeProvider]]))
--- reframe-tut/src/app/nav/views/authenticated.cljs
+++ reframe-tut/src/app/nav/views/authenticated.cljs
@@ -4,29 +4,34 @@
             [app.nav.views.nav-item :refer [nav-item]]))
 
 (defn authenticated []
-  (let [nav-items [{:id       :saved
-                    :name     "Saved"
-                    :dispatch #(rf/dispatch [:set-active-nav :saved])
-                    :href     "#saved"}
-                   {:id       :recipies
-                    :name     "Recipies"
-                    :dispatch #(rf/dispatch [:set-active-nav :recipies])
-                    :href     "#recipies"}
-                   {:id       :inboxes
-                    :name     "Inbox"
-                    :dispatch #(rf/dispatch [:set-active-nav :inboxes])
-                    :href     "#inbox"}
-                   {:id       :become-a-chef
-                    :name     "Chef"
-                    :dispatch #(rf/dispatch [:set-active-nav :become-a-chef])
-                    :href     "#become-a-chef"}
-                   {:id       :profile
-                    :name     "Profile"
-                    :dispatch #(rf/dispatch [:set-active-nav :profile])
-                    :href     "#profile"}]]
+  (let [active-nav @(rf/subscribe [:active-nav])
+        nav-items  [{:id       :saved
+                     :name     "Saved"
+                     :dispatch #(rf/dispatch [:set-active-nav :saved])
+                     :href     "#saved"}
+                    {:id       :recipies
+                     :name     "Recipies"
+                     :dispatch #(rf/dispatch [:set-active-nav :recipies])
+                     :href     "#recipies"}
+                    {:id       :inboxes
+                     :name     "Inbox"
+                     :dispatch #(rf/dispatch [:set-active-nav :inboxes])
+                     :href     "#inbox"}
+                    {:id       :become-a-chef
+                     :name     "Chef"
+                     :dispatch #(rf/dispatch [:set-active-nav :become-a-chef])
+                     :href     "#become-a-chef"}
+                    {:id       :profile
+                     :name     "Profile"
+                     :dispatch #(rf/dispatch [:set-active-nav :profile])
+                     :href     "#profile"}]]
     [:> Box {:display         "flex"
              :justify-content "flex-end"
              :py              1}
-     (for [item nav-items]
-       ^{:key (:id item)}
-       [nav-item item])]))
+     (for [{:keys [id name href dispatch]} nav-items]
+       [nav-item {:key        id
+                  :id         id
+                  :name       name
+                  :href       href
+                  :dispatch   dispatch
+                  :active-nav active-nav}])]))
