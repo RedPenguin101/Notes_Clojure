6f46ee477eda56fd8594a76fdc2604a313a55240
RedPenguin101 <allaboutthatmace1789@gmail.com>
2020-04-25 11:02:53 +0100

    v13 form 2 component

--- reframe-tut/src/app/auth/views/log_in.cljs
+++ reframe-tut/src/app/auth/views/log_in.cljs
@@ -6,22 +6,23 @@
 (defn log-in []
   (let [initial-values {:email "" :password ""}
         values         (r/atom initial-values)]
-    [:> Row {:justify-content "center"}
-     [:> Col {:xs 12 :sm 6}]
-     [page-nav {:center "Log In"}]
-     [:> FormGroup
-      [:> Label {:html-for :email} "Email"]
-      [:> Input {:control   true
-                 :id        :email
-                 :type      "email"
-                 :value     (:email @values)
-                 :on-change #(swap! values assoc :email (.. % -target -value))}]]
-     [:> FormGroup
-      [:> Label {:html-for :password} "Password"]
-      [:> Input {:control   true
-                 :id        :password
-                 :type      "password"
-                 :value     (:password @values)
-                 :on-change #(swap! values assoc :password (.. % -target -value))}]]
-     ])
+    (fn []
+      [:> Row {:justify-content "center"}
+       [:> Col {:xs 12 :sm 6}]
+       [page-nav {:center "Log In"}]
+       [:> FormGroup
+        [:> Label {:html-for :email} "Email"]
+        [:> Input {:control   true
+                   :id        :email
+                   :type      "email"
+                   :value     (:email @values)
+                   :on-change #(swap! values assoc :email (.. % -target -value))}]]
+       [:> FormGroup
+        [:> Label {:html-for :password} "Password"]
+        [:> Input {:control   true
+                   :id        :password
+                   :type      "password"
+                   :value     (:password @values)
+                   :on-change #(swap! values assoc :password (.. % -target -value))}]]
+       ]))
   )
