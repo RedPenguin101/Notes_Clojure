8f01f635a329a1b63a72d9243cbc3b37c1b8363a
RedPenguin101 <allaboutthatmace1789@gmail.com>
2020-08-11 21:43:31 +0100

    Markdownify 3.1 Add copy-to-clipboard and copy button

--- markdownify/notes.md
+++ markdownify/notes.md
@@ -103,3 +103,27 @@ var showdown  = require('showdown'),
   
 [:div (md->html @markdown)]
 ```
+## Lecture 4: adding copy buttons
+
+```javascript
+const copyToClipboard = str => {
+  const el = document.createElement('textarea');  
+  el.value = str;  
+  el.setAttribute('readonly', '');
+  el.style.position = 'absolute';
+  el.style.left = '-9999px';
+  document.body.appendChild(el);
+  const selected =            
+    document.getSelection().rangeCount > 0 
+      ? document.getSelection().getRangeAt(0)
+      : false;
+  el.select();   
+  document.execCommand('copy');
+  document.body.removeChild(el);
+  if (selected) {
+    document.getSelection().removeAllRanges();
+    document.getSelection().addRange(selected);
+  }
+};
+```
+Just transcribe to cljs
--- markdownify/src/main/markdownify/main.cljs
+++ markdownify/src/main/markdownify/main.cljs
@@ -9,6 +9,45 @@
 (defn md->html [md]
   (.makeHtml showdown-converter md))
 
+"https://hackernoon.com/copying-text-to-clipboard-with-javascript-df4d4988697f"
+(defn copy-to-clipboard [s]
+  (let [el       (.createElement js/document "textarea")
+        selected (when (pos? (-> js/document .getSelection .-rangeCount))
+                   (-> js/document .getSelection (.getRangeAt 0)))]
+    (set! (.-value el) s)
+    (.setAttribute el "readonly" "")
+    (set! (-> el .-style .-position) "absolute")
+    (set! (-> el .-style .-left) "-9999px")
+    (-> js/document .-body (.appendChild el))
+    (.select el)
+    (.execCommand js/document "copy")
+    (-> js/document .-body (.removeChild el))
+    (when selected
+      (-> js/document .getSelection .removeAllRanges)
+      (-> js/document .getSelection (.addRange selected)))))
+
+"
+const copyToClipboard = str => {
+  const el = document.createElement('textarea');
+  el.value = str;
+  el.setAttribute('readonly', '');
+  el.style.position = 'absolute';
+  el.style.left = '-9999px';
+  document.body.appendChild(el);
+  const selected =
+    document.getSelection().rangeCount > 0
+      ? document.getSelection().getRangeAt(0)
+      : false;
+  el.select();
+  document.execCommand('copy');
+  document.body.removeChild(el);
+  if (selected) {
+    document.getSelection().removeAllRanges();
+    document.getSelection().addRange(selected);
+  }
+};
+"
+
 (defn app []
   [:div
    [:h1 "Markdownify"]
@@ -21,7 +60,9 @@
                  :value     @markdown
                  :style     {:resize "none"
                              :height "500px"
-                             :width  "100%"}}]]
+                             :width  "100%"}}]
+     [:button {:on-click #(copy-to-clipboard @markdown)}
+      "Copy Markdown"]]
     [:div
      {:style {:flex         "1"
               :padding-left "2em"}}
