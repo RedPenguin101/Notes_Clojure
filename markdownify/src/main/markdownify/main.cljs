3e0723947b8c91a41d52a47e146eb33fb14a2bd1
RedPenguin101 <allaboutthatmace1789@gmail.com>
2020-08-11 21:57:31 +0100

    Markdownify 4.1 add html state and textarea

--- markdownify/src/main/markdownify/main.cljs
+++ markdownify/src/main/markdownify/main.cljs
@@ -3,11 +3,15 @@
             ["showdown" :as showdown]))
 
 (defonce markdown (r/atom ""))
+(defonce html     (r/atom ""))
 
 (defonce showdown-converter (showdown/Converter. ))
 
 (defn md->html [md]
   (.makeHtml showdown-converter md))
+
+(defn html->md [html]
+  (.makeMarkdown showdown-converter html))
 
 "https://hackernoon.com/copying-text-to-clipboard-with-javascript-df4d4988697f"
 (defn copy-to-clipboard [s]
@@ -34,7 +38,9 @@
     [:div
      {:style {:flex "1"}}
      [:h2 "Markdown"]
-     [:textarea {:on-change #(reset! markdown (-> % .-target .-value))
+     [:textarea {:on-change (fn [event]
+                              (reset! markdown (-> event .-target .-value))
+                              (reset! html (md->html @markdown)))
                  :value     @markdown
                  :style     {:resize "none"
                              :height "500px"
@@ -46,20 +52,34 @@
                           :border-radius    10}}
       "Copy Markdown"]]
     [:div
+     {:style {:flex "1"}}
+     [:h2 "HTML"]
+     [:textarea {:on-change (fn [event]
+                              (reset! html (-> event .-target .-value))
+                              (reset! markdown (html->md @html)))
+                 :value     @html
+                 :style     {:resize "none"
+                             :height "500px"
+                             :width  "100%"}}]
+     [:button {:on-click #(copy-to-clipboard @html)
+               :style    {:background-color :green
+                          :padding          "1em"
+                          :color            :white
+                          :border-radius    10}}
+      "Copy html"]]
+    [:div
      {:style {:flex         "1"
               :padding-left "2em"}}
      [:h2 "HTML preview"]
      [:div {:style                   {:height "500px"}
-            :dangerouslySetInnerHTML {:__html (md->html @markdown)}}]
-     [:button {:on-click #(copy-to-clipboard (md->html @markdown))
+            :dangerouslySetInnerHTML {:__html @html}}]
+     [:button {:on-click #(copy-to-clipboard @html)
                :style    {:background-color :green
                           :padding          "1em"
                           :color            :white
                           :border-radius    10}}
       "Copy HTML"]]
-    #_[:div
-       [:h2 "Raw HTML"]
-       [:div (md->html @markdown)]]]])
+    ]])
 
 (defn mount! []
   (r/render [app]
