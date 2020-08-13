04c6c2faed9f4eb886c585ba52d4e612d83e77fd
RedPenguin101 <allaboutthatmace1789@gmail.com>
2020-08-13 22:12:32 +0100

    normand reagent 4

--- normand/src/main/normand/hiccup.cljs
+++ normand/src/main/normand/hiccup.cljs
@@ -1,18 +1,25 @@
 (ns normand.hiccup
   (:require [reagent.core :as r]))
 
-"How to deal with HTML attributes that are either there or not there - like checked"
+"Attributes in hiccup"
+"common to style inline in react and reagent"
+"Use a map"
+"inline is good because you can use stateful properties"
 
 (defn hiccup-panel []
-  (let [title    "Show"
-        dontshow nil]
+  (let [title        "Show"
+        dontshow     nil
+        border-color "red"]
     [:div
      nil ;; nil/false is ignored - useful
      (when title [:h1 title])
      false
      (when dontshow [:h1 "Don't show!"])
-     [:div {:class "content"}
-      [:p "this is in the content class div"]]
+     [:div {:class "content"
+            :style {:margin-left 100}} ;; integer is interpreted as px. Else use string
+      [:p {:style {:border (when border-color
+                             (str "1px solid " border-color))}}
+       "this is in the content class div"]]
      [:p [:a {:href "#/"} "Home"]]
      [:input {:type    :checkbox
               :checked title}]
@@ -21,4 +28,3 @@
      [:input {:type    :checkbox
               :checked {}}]]))
 
-":checked will show up in your html if the value for the key is truthy"
