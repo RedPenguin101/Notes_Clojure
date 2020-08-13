d11f6767ed7695de1b8d8c52188bab4129528283
RedPenguin101 <allaboutthatmace1789@gmail.com>
2020-08-13 22:02:34 +0100

    normand reagent 3

--- normand/src/main/normand/hiccup.cljs
+++ normand/src/main/normand/hiccup.cljs
@@ -1,6 +1,8 @@
 (ns normand.hiccup
   (:require [reagent.core :as r]))
 
+"How to deal with HTML attributes that are either there or not there - like checked"
+
 (defn hiccup-panel []
   (let [title    "Show"
         dontshow nil]
@@ -11,4 +13,12 @@
      (when dontshow [:h1 "Don't show!"])
      [:div {:class "content"}
       [:p "this is in the content class div"]]
-     [:p [:a {:href "#/"} "Home"]]]))
+     [:p [:a {:href "#/"} "Home"]]
+     [:input {:type    :checkbox
+              :checked title}]
+     [:input {:type    :checkbox
+              :checked dontshow}]
+     [:input {:type    :checkbox
+              :checked {}}]]))
+
+":checked will show up in your html if the value for the key is truthy"
