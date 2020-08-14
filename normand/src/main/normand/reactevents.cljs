2ad7600778280b18fb7f745f28a803571b3921b7
RedPenguin101 <allaboutthatmace1789@gmail.com>
2020-08-14 19:31:12 +0100

    normand reagent 11 subcomponents

--- normand/src/main/normand/reactevents.cljs
+++ normand/src/main/normand/reactevents.cljs
@@ -1,25 +1,25 @@
 (ns normand.reactevents
   (:require [reagent.core :as r]))
 
-"click events propogate outwards - in the below, when clicking the some content para, both the Inner Div and wrapper div with be triggered"
+"Break big components into subcomponents when they get too big"
+
+;; this returns a data structure
+(defn form1subcomponent [link-text]
+  [:p [:a {:href     "#/"
+           :on-click (fn [e]
+                       (.preventDefault e)
+                       ;; this prevents the default behaviour, but doesn't stop
+                       ;; the propagation
+                       (js/console.log "Link"))} link-text]])
 
 (defn react-panel []
   [:div.top
-   {:on-click (fn [event]
-                ;; This won't get triggered if we have this method
-                (.stopPropagation event))}
    [:div.wrapper
     {:on-click (fn [event]
                  (js/console.log "Wrapper gets triggered 2nd, because of propagation"))}
     [:div.inner
      {:on-click (fn [event]
-                  (js/console.log (.-target event))
-                  ;; the p that was clicked is the target
                   (js/console.log "Inner gets triggered first"))}
-     [:p [:a {:href     "#/"
-              :on-click (fn [e]
-                          (.preventDefault e)
-                          ;; this prevents the default behaviour, but doesn't stop
-                          ;; the propagation
-                          (js/console.log "Link"))}"some content"]]
+     [form1subcomponent "passed in text"] ;; this will get turned into a react component
+     (form1subcomponent "not a component") ;; this isn't a react component
      [:p "Other content"]]]])
