659d700dc37bdaf836f0798e3abc9a7bb89a4158
RedPenguin101 <allaboutthatmace1789@gmail.com>
2020-08-14 19:24:46 +0100

    normand reagent 10 events and propagation

--- normand/src/main/normand/main.cljs
+++ normand/src/main/normand/main.cljs
@@ -1,12 +1,18 @@
 (ns normand.main
   (:require [reagent.core :as r]
             [reagent.dom :as rd]
-            [normand.hiccup :refer [hiccup-panel]]))
+            [normand.hiccup :refer [hiccup-panel]]
+            [normand.reactevents :as rp]))
 
 (defn app []
   [:div
    [:h1 "hello world 2"]
-   [hiccup-panel]])
+   [:div
+    [:h2 "React Panel"]
+    [rp/react-panel]]
+   [:div
+    [:h2 "Hiccup Panel"]
+    [hiccup-panel]]])
 
 (defn mount []
   (rd/render [app]
