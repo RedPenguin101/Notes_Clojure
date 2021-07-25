e25160a58fffa2c32b758d6a8c4543a252a520ea
RedPenguin101 <allaboutthatmace1789@gmail.com>
2021-07-25 12:49:41 +0100

    client sente

--- sente_example/ws_server/src/main.clj
+++ sente_example/ws_server/src/main.clj
@@ -1,18 +1,41 @@
 (ns main
   (:require [org.httpkit.server :refer [run-server]]
-            [compojure.core :as comp :refer [defroutes]]
-            [clojure.data.json :as json]))
+            [compojure.core :as comp :refer [defroutes GET POST]]
+            [clojure.data.json :as json]
+            [taoensso.sente :as sente]
+            [ring.middleware.keyword-params]
+            [ring.middleware.params]
+            [ring.middleware.session]
+            [ring.middleware.anti-forgery :refer [wrap-anti-forgery]]
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [taoensso.sente.server-adapters.http-kit :refer (get-sch-adapter)]))
 
 (defonce server (atom nil))
 
-(defroutes routes
-  (comp/GET "/" [] {:status 200
-                    :body (json/write-str {"Hello" "World"})
-                    :headers {"Content-Type" "application/json"
-                              "Access-Control-Allow-Origin" "http://localhost:9090"}})
-  ())
+(let [{:keys [ch-recv send-fn connected-uids
+              ajax-post-fn ajax-get-or-ws-handshake-fn]}
+      (sente/make-channel-socket! (get-sch-adapter) {})]
 
-(def app (-> routes))
+  (def ring-ajax-post                ajax-post-fn)
+  (def ring-ajax-get-or-ws-handshake ajax-get-or-ws-handshake-fn)
+  (def ch-chsk                       ch-recv) ; ChannelSocket's receive channel
+  (def chsk-send!                    send-fn) ; ChannelSocket's send API fn
+  (def connected-uids                connected-uids) ; Watchable, read-only atom
+  )
+
+(defroutes routes
+  (GET "/" [] {:status 200
+               :body (json/write-str {"Hello" "World"})
+               :headers {"Content-Type" "application/json"
+                         "Access-Control-Allow-Origin" "http://localhost:9090"}})
+  (GET  "/chsk" req (ring-ajax-get-or-ws-handshake req))
+  (POST "/chsk" req (ring-ajax-post req)))
+
+(def app (-> routes
+             (wrap-defaults site-defaults)
+             ring.middleware.keyword-params/wrap-keyword-params
+             ring.middleware.params/wrap-params
+             ring.middleware.session/wrap-session))
 
 (defn start []
   (reset! server (run-server (fn [req] (app req)) {:port 3000 :join? false})))
